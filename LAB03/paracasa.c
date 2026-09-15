223979400b6440c8547df82947549a6dc7086d11
Ana Carolina <aninhaana2013.pb@gmail.com>
2026-09-15 18:45:23 -0300

    Refactor paracasa.c for improved readability and functionality
    Refactor code to initialize variables and configure interrupt handling. Update display functions for better clarity and functionality.

--- LAB03/paracasa.c
+++ LAB03/paracasa.c
@@ -4,7 +4,8 @@
 #include <avr/interrupt.h>
 #include <util/delay.h>
 
-volatile unsigned int contador_de_pulsos;
+volatile unsigned int contador_de_pulsos = 0;
+unsigned long rpm = 0; 
 
 volatile unsigned char *ponteiro_portb = (volatile unsigned char *) 0x25; // PORTB (pinos 8 a 13)
 volatile unsigned char *ponteiro_portc = (volatile unsigned char *) 0x28; // PORTC (pinos A0 a A5)
@@ -14,22 +15,33 @@ volatile unsigned char *ponteiro_ddrb  = (volatile unsigned char *) 0x24; // DDR
 volatile unsigned char *ponteiro_ddrc  = (volatile unsigned char *) 0x27; // DDRC
 volatile unsigned char *ponteiro_ddrd  = (volatile unsigned char *) 0x2A; // DDRD
 
-config(){
+volatile unsigned char *ponteiro_PCMSK1  = (volatile unsigned char *) 0x6C; // PCMSK1
+volatile unsigned char *ponteiro_PCICR   = (volatile unsigned char *) 0x68; // PCICR
+volatile unsigned char *ponteiro_pcifr   = (volatile unsigned char *) 0x3B; // PCIFR
+
+void config(void){
     cli();
     
-    //configurar os leds como saida e o A3 como entrada 
-    // habilitar os pinos de interrupção (os que têm que ser habilitados estão no mapeamento)
+    *ponteiro_ddrb |= 0x20;  // Liga o BIT 5 do ddrb do led para 1 (SAIDA)
+    *ponteiro_ddrc &= ~0x08; // Limpa o bit 3 do DDRC (configura como ENTRADA)
+    *ponteiro_portc |= 0x08; // Ativa o pull-up interno no PC3
+ 
+    // Habilita o bit 3 do PCMSK1 (0x6C)
+    *ponteiro_PCMSK1 |= 0x08;
+
+    // Habilita o bit 1 do PCICR (0x68)
+    *ponteiro_PCICR |= 0x02;
 
     sei();
 }
 
 // Rotina de serviço de interrupção
 ISR(PCINT1_vect) {
-   int contador_de_pulsos++;
+    contador_de_pulsos++;
+   
 }
 
 void forma_dezena(unsigned char digito) {
-
     // Apagar todos os displays
     *ponteiro_portb &= ~0x05; // Limpa os bits 0 e 2      0b00000101
     *ponteiro_portc &= ~0x12; // Limpa os bits 1 e 4      0b00010010
@@ -98,7 +110,6 @@ void forma_dezena(unsigned char digito) {
 }
 
 void forma_centena(unsigned char digito) {
-
     // Apagar todos os displays
     *ponteiro_portb &= ~0x2A; // Limpa os bits 1, 3 e 5  (0b00101010)
     *ponteiro_portc &= ~0x11; // Limpa os bits 0 e 5     (0b00010001)
@@ -167,7 +178,34 @@ void forma_centena(unsigned char digito) {
     }
 }
 
-main (){
+int main (void){
+    int numero_de_pulsos = 0;
+    int dezena = 0;
+    int centena = 0;
 
-    
+    config();
+
+    while(1){  
+     
+        cli();
+        contador_de_pulsos = 0;
+        sei();
+
+     
+        _delay_ms(1000);
+
+        cli();
+        numero_de_pulsos = contador_de_pulsos;
+        sei();
+
+        rpm = ((unsigned long)numero_de_pulsos * 60) / 49;
+
+        centena = (rpm / 100) % 10;
+        dezena  = (rpm / 10) % 10;
+
+        forma_dezena(dezena);
+        forma_centena(centena);
+    }
+
+    return 0;
 }
